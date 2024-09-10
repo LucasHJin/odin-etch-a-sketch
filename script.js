eb77edf8f9fc9aa90ae40429161de4ff60546d52
Lucas Jin <kblazer20@gmail.com>
2024-09-10 11:32:38 -0400

    Added hover effect

--- script.js
+++ script.js
@@ -2,6 +2,17 @@ container = document.querySelector(".container");
 
 for (let i=0; i<256; i++) {
     div = document.createElement("div");
+    div.classList.add("tile");
     container.appendChild(div);
 }
 
+tiles = document.querySelectorAll(".tile");
+
+for (const tile of tiles) {
+    tile.addEventListener("mouseover", function () {
+        tile.style.backgroundColor = "pink";
+    }); 
+    tile.addEventListener("mouseout", function () {
+        tile.style.backgroundColor = "red";
+    }); 
+}
